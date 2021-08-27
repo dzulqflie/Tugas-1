47ff7e11713e2e9d3ce2a9930b557da213ec0fe3
dzulqflie <89216679+dzulqflie@users.noreply.github.com>
2021-08-27 10:34:19 +0700

    Create tugas2.php

---
+++ tugas2.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Nama lengkap</title>
+</head>
+<body>
+<b>Ceritakan tentang diri anda</b>
+<body bgcolor="yellow" text="green">
+<h1> Nama Lengkap</h1>
+<hr width="100%">
+</body>
+</html>
